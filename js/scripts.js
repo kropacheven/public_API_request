1b0c144867803fef9e05aa4675a489d3c34dc7e9
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-02-06 22:14:03 +0300

    Structuring the project

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+// 1. Dinamicly add HTML content to the main gallery:
+
+// 2. Arranging CSS (in styles.css file):
+
+// 3. Get and display 12 random users from The Random User Generator API:
+
+// 4. Create a modal window:
+
+
+
+
+
+// ---------------------------------------  Extra Credit --------------------------- //
+
+// 1. Programm search bar element:
+
+// 2. Programm modal toggle:
+
+// 3. Structure, style and CSS:
