d2e4ac93abd523c8ac65525ca5b8fc79c3c8c971
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-02-09 23:25:39 +0300

    Fetching 1 random person on the page

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,29 @@
 
 // 3. Get and display 12 random users from The Random User Generator API:
 
+// a. Fetch data from random employee API:
+fetch('https://randomuser.me/api/')
+    .then( response => response.json() )
+    .then( data => getData(data.results[0]) )
+
+// b. Function to handle data from API:
+function getData(data) {
+    const html = `
+    <div class="card">
+    <div class="card-img-container">
+        <img class="card-img" src="${data.picture.thumbnail}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+        <p class="card-text">${data.email}</p>
+        <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+    </div>
+</div>
+    `;
+    document.getElementById('gallery').innerHTML = html;
+}
+
+
 // 4. Create a modal window:
 
 
